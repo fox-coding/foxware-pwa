e92fe4d31420922a1129c8b314ce7d8f6d38372b
Konstantin Pfafenrod <pfafenrod@gmail.com>
2022-06-23 09:42:33 +0200

    abo Slider + Gallery v2

--- shopware-pwa.config.js
+++ shopware-pwa.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  shopwareEndpoint: "http://localhost",
-  shopwareAccessToken: "SWSCNNHYBEDJAKL2NWXIRUZ0EG",
-  shopwareDomainsAllowList: ["http://shopware6.abo-dev.de", "http://localhost" ]
-};
 // module.exports = {
-//   shopwareEndpoint: "https://pwa-demo-api.shopware.com/trunk",
-//   shopwareAccessToken: "SWSC40-LJTNO6COUEN7CJMXKLA",
-//   shopwareDomainsAllowList: ["https://pwa-demo-api.shopware.com/trunk/", ]
+//   shopwareEndpoint: "http://localhost",
+//   shopwareAccessToken: "SWSCNNHYBEDJAKL2NWXIRUZ0EG",
+//   shopwareDomainsAllowList: ["http://shopware6.abo-dev.de", "http://localhost" ]
 // };
+module.exports = {
+  shopwareEndpoint: "https://pwa-demo-api.shopware.com/trunk",
+  shopwareAccessToken: "SWSC40-LJTNO6COUEN7CJMXKLA",
+  shopwareDomainsAllowList: ["https://pwa-demo-api.shopware.com/trunk/", ]
+};
--- src/components/organisms/AboImageSlider.vue
+++ src/components/organisms/AboImageSlider.vue
@@ -1,72 +1,73 @@
 <template>
   <div>
-    <div ref="glide" class="glide">
-      <div class="glide__track" data-glide-el="track">
-        <ul class="glide__slides sf-hero__slides">
-          <!--@slot default slot for SfHeroItem tags -->
-          <slot />
-        </ul>
+    <div
+    class="relative">
+      <div ref="glide" class="glide">
+        <div class="glide__track" data-glide-el="track">
+          <ul class="glide__slides sf-hero__slides">
+            <!--@slot default slot for SfHeroItem tags -->
+            <slot />
+          </ul>
+        </div>
+      </div>
+      <div v-if="numberOfPages > 1" class="sf-hero__control--left">
+        <!--@slot slot for icon moving to the previous item -->
+        <slot name="prev" v-bind="{ go: () => go('prev') }">
+          <SfArrow
+            class="sf-arrow sf-arrow--transparent"
+            aria-label="previous"
+            data-testid="hero-prev-button"
+            @click.stop="go('prev')"
+          />
+        </slot>
+      </div>
+      <div v-if="numberOfPages > 1" class="sf-hero__control--right">
+        <!-- @slot slot for icon moving to the next item  -->
+        <slot name="next" v-bind="{ go: () => go('next') }">
+          <SfArrow
+            class="sf-arrow sf-arrow--right sf-arrow--transparent"
+            aria-label="next"
+            data-testid="hero-next-button"
+            @click.stop="go('next')"
+          />
+        </slot>
+      </div>
+      <div v-if="numberOfPages > 1">
+        <!--@slot custom markup for pagination bullets -->
+        <slot name="bullets" v-bind="{ numberOfPages, page, go }">
+          <SfBullets
+            :total="numberOfPages"
+            :current="page - 1"
+            :class="'absolute left-0 right-0 m-auto justify-center items-center'"
+            data-testid="hero-bullets"
+            @click="go($event)"
+          />
+        </slot>
       </div>
     </div>
-    <div v-if="numberOfPages > 1" class="sf-hero__control--left">
-      <!--@slot slot for icon moving to the previous item -->
-      <slot name="prev" v-bind="{ go: () => go('prev') }">
-        <SfArrow
-          class="sf-arrow sf-arrow--transparent"
-          aria-label="previous"
-          data-testid="hero-prev-button"
-          @click.stop="go('prev')"
-        />
-      </slot>
-    </div>
-    <div v-if="numberOfPages > 1" class="sf-hero__control--right">
-      <!-- @slot slot for icon moving to the next item  -->
-      <slot name="next" v-bind="{ go: () => go('next') }">
-        <SfArrow
-          class="sf-arrow sf-arrow--right sf-arrow--transparent"
-          aria-label="next"
-          data-testid="hero-next-button"
-          @click.stop="go('next')"
-        />
-      </slot>
-    </div>
-    <div v-if="numberOfPages > 1">
-      <!--@slot custom markup for pagination bullets -->
-      <slot name="bullets" v-bind="{ numberOfPages, page, go }">
-        <SfBullets
-          :total="numberOfPages"
-          :current="page - 1"
-          data-testid="hero-bullets"
-          @click="go($event)"
-        />
-      </slot>
-    </div>
 
-
-
-
-    <div class="absolute w-1/5 h-full left-0 top-0 overflow-hidden">
-      <div 
+    <div class="absolute w-1/5 h-full left-0 top-0 overflow-auto">
+      <div
         class="w-24 h-24 border border-gray-300 rounded-sm m-2"
         v-for="(slide, index) in slides"
         :key="slide.mediaId"
-        :style="{'background': `url(${slide.mediaUrl})`,'background-size':'contain',}" 
-        @click.stop="go(index)">
+        :style="{
+          background: `url(${slide.mediaUrl})`,
+          'background-size': 'contain',
+        }"
+        @click.stop="go(index)"
+      >
       </div>
     </div>
-
-
-
-
   </div>
 </template>
 <script>
-import Vue from "vue";
+import Vue from "vue"
 import AboHeroItem from "@/components/atoms/AboHeroItem.vue"
-import { SfBullets, SfArrow } from "@storefront-ui/vue";
+import { SfBullets, SfArrow } from "@storefront-ui/vue"
 
-import Glide from "@glidejs/glide";
-Vue.component("AboHeroItem", AboHeroItem);
+import Glide from "@glidejs/glide"
+Vue.component("AboHeroItem", AboHeroItem)
 export default {
   name: "AboImageSlider",
   components: {
@@ -81,7 +82,6 @@ export default {
       type: Object,
       default: () => ({}),
     },
-    
   },
   data() {
     return {
@@ -94,54 +94,54 @@ export default {
         perView: 1,
         gap: 0,
       },
-    };
+    }
   },
   computed: {
     mergedOptions() {
       return {
         ...this.defaultOptions,
         ...this.sliderOptions,
-      };
+      }
     },
     numberOfPages() {
       return this.$slots.default
         ? this.$slots.default.filter((slot) => slot.tag).length
-        : 0;
+        : 0
     },
     page() {
       if (this.glide) {
-        return this.glide.index + 1;
+        return this.glide.index + 1
       }
-      return 1;
+      return 1
     },
   },
   mounted() {
     if (this.numberOfPages > 1) {
       this.$nextTick(() => {
-        if (!this.$slots.default) return;
-        const glide = new Glide(this.$refs.glide, this.mergedOptions);
-        glide.mount();
-        this.glide = glide;
-      });
+        if (!this.$slots.default) return
+        const glide = new Glide(this.$refs.glide, this.mergedOptions)
+        glide.mount()
+        this.glide = glide
+      })
     }
   },
   methods: {
     go(direct) {
-      if (!this.glide) return;
+      if (!this.glide) return
       switch (direct) {
         case "prev":
-          this.glide.go("<");
-          break;
+          this.glide.go("<")
+          break
         case "next":
-          this.glide.go(">");
-          break;
+          this.glide.go(">")
+          break
         default:
-          this.glide.go(`=${direct}`);
-          break;
+          this.glide.go(`=${direct}`)
+          break
       }
     },
   },
-};
+}
 </script>
 <style lang="scss">
 @import "~@storefront-ui/shared/styles/components/organisms/SfHero.scss";
