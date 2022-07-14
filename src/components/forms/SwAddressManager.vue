a29453e7e9317d240922ea19f16a3128c8634b4d
Konstantin Pfafenrod <pfafenrod@gmail.com>
2022-07-14 13:03:43 +0200

    Adressauswahl

---
+++ src/components/forms/SwAddressManager.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="p-6 flex justify-between flex-col">
+    <div class="text-base font-bold mb-2">{{ titleText || $t('Shipping address') }}</div>
+    <div class="grow text-sm mb-2" v-if="activeAddress">
+      <div class="text-sm">
+        <div>{{ activeAddress.firstName }} {{ activeAddress.lastName }}</div>
+        <div>{{ activeAddress.street }}</div>
+        <div>{{ activeAddress.zipcode }} {{ activeAddress.city }}</div>
+        <div>{{ activeAddress.country.name }}</div>
+        <div>{{ activeAddress.phoneNumber }}</div>
+      </div>
+    </div>
+    <button class="bg-abo-gray font-bold hover:text-abo-primary" v-if="activeAddress" @click="
+          isModalOpen = true
+          isEditModeOpen = true
+        ">
+        {{ $t("Edit") }}
+    </button>
+    <SfModal
+      class=""
+      :title="$t('Edit address')"
+      :visible="isModalOpen"
+      @close="isModalOpen = false"
+    >
+      <SfHeading
+        :title="$t('Edit address')"
+        class=""
+        :level="4"
+      />
+      <div v-if="isEditModeOpen" class="">
+        <div
+          v-for="address in addresses"
+          :key="address.id"
+          class=""
+        >
+          <SfAddressPicker
+            :selected="activeAddress.id"
+            @change="onAddressChange"
+          >
+            <SfAddress :name="address.id">
+              <span>{{ address.firstName }} {{ address.lastName }}</span>
+              <p>{{ address.street }}</p>
+              <span>{{ address.zipcode }} {{ address.city }}</span>
+              <span>{{ address.country.name }}</span>
+              <span>{{ address.phoneNumber }}</span>
+            </SfAddress>
+          </SfAddressPicker>
+        </div>
+        <SwButton
+          class=""
+          @click="
+            isModalOpen = true
+            isEditModeOpen = false
+          "
+        >
+          {{ $t("Add new") }}
+        </SwButton>
+      </div>
+      <SwAddressForm
+        v-else
+        @success="onAddressSave"
+        @cancel="isModalOpen = false"
+      />
+    </SfModal>
+  </div>
+</template>
+
+<script>
+import {
+  SfHeading,
+  SfRadio,
+  SfModal,
+  SfAddressPicker,
+} from "@storefront-ui/vue"
+import { useSessionContext, useUser } from "@shopware-pwa/composables"
+import SwButton from "@/components/atoms/SwButton.vue"
+import { ref, watch } from "@vue/composition-api"
+import SwAddressForm from "@/components/forms/SwAddressForm.vue"
+// import SwCheckbox from '@/components/atoms/SwCheckbox.vue'
+
+export default {
+  name: "SwAddressManager",
+  components: {
+    SfRadio,
+    // SwCheckbox,
+    SwButton,
+    SfModal,
+    SfAddressPicker,
+    SwAddressForm,
+    SfHeading,
+  },
+  props: {
+    addresses: {
+      type: Array,
+      default: [],
+    },
+    activeAddress: {
+      type: Object,
+      default: null,
+    },
+    titleText: {
+      type: String,
+      default: null,
+    },
+    subtitleText: {
+      type: String,
+      default: null,
+    },
+  },
+  setup(props, { root, emit }) {
+    const isModalOpen = ref(false)
+    const isEditModeOpen = ref(false)
+
+    const onAddressSave = (addressId) => {
+      isModalOpen.value = false
+      emit("added", addressId)
+    }
+
+    function onAddressChange(value) {
+      emit("change", value)
+    }
+
+    return {
+      isModalOpen,
+      onAddressSave,
+      isEditModeOpen,
+      onAddressChange,
+    }
+  },
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,33 +2,19 @@ module.exports = {
   content: [
     "./sw-plugins/my-local-plugin/myLocalPlugin.vue",
     "./src/pages/index.vue",
-    "./src/components/SwHeader.vue",
-    "./src/components/SwTopNavigation.vue",
-    "./src/components/SwSearchBar.vue",
-    "./src/components/SwHeaderIcons.vue",
-    "./src/components/SwFooter.vue",
-    "./src/components/organisms/SwFooterNavigation.vue",
-    "./src/components/SwProductDescription.vue",
+    "./src/components/**/*.{vue,js,ts,jsx,tsx}",
     "./cms/blocks/CmsBlockImageHeroHeading.vue",
     "./cms/elements/CmsElementAboGallery.vue",
     "./src/components/organisms/AboImageSlider.vue",
     "./cms/elements/CmsElementBgbox.vue",
-    "./src/pages/aboTeam.vue",
-    ".shopware-pwa/pwa-bundles-assets/abo-team/main.vue",
-    ".shopware-pwa/pwa-bundles-assets/abo-contact/main.vue"
+    ".shopware-pwa/pwa-bundles-assets/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    typography: {
-      default: {
-        css: {
-          'div.noprose': false,
-        }
-      }
-    },
     extend: {
       colors: {
         'abo-primary': '#006ab3',
         'abo-secondary': '#009fe3',
+        'abo-gray': '#e3e3e3',
         'abo-transparentWhite': '#ffffffde'
       }
     },
